b1f5104904785daf7d90eb0b8dc2e4dd0426ecbd
Ryanfrump <ryanfrump@gmail.com>
2024-07-17 07:55:55 -0600

    this is dumb

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ETA-calculator-3/",
+  base: "/ETA-Calculator-3/",
 });
